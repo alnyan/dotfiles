ebc4cee353293fc057e38120f5a0dd96131381bd
alnyan <qShadowp@gmail.com>
2018-04-04 15:26:21 +0300

    Updated setup.sh: Added file linking/copying

--- setup.sh
+++ setup.sh
@@ -1,10 +1,31 @@
 #!/bin/env bash
 
 deps="zsh nvim polybar i3 xsecurelock dmenu escrotum xbacklight amixer feh i3-msg"
+pairs="./i3/config:.config/i3/config ./i3/i3rc.sh:.config/i3/i3rc.sh ./i3/polybar.sh:.config/i3/polybar.sh ./i3/workspace-1.json:.config/i3/workspace-1.json ./zshrc:.zshrc ./Xresources:.Xresources ./polybar/config:.config/polybar/config ./nvim/init.vim:.config/nvim/init.vim"
 
 dep_fail() {
     echo "MISSING"
     exit 1
+}
+
+file_install() {
+    k=$(echo ${p} | cut -d: -f1 | xargs readlink -f);
+    v="${HOME}/$(echo ${p} | cut -d: -f2)";
+    if [[ ${2} == "hard" ]]; then
+        echo "-- COPY \"${k}\" to \"${v}\"";
+        if [[ ! -f "${v}" ]]; then
+            cp ${k} ${v} || exit 1;
+        else
+            echo "  Already exists";
+        fi
+    else
+        echo "-- LINK \"${v}\" to \"${k}\"";
+        if [[ ! -f "${v}" ]]; then
+            ln -s ${k} ${v} || exit 1;
+        else
+            echo "  Already exists";
+        fi;
+    fi;
 }
 
 # Check dependencies
@@ -23,3 +44,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 echo "Installing vim-plug:"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install dotfiles
+
+for p in ${pairs}; do
+    file_install ${p} ${1};
+done
